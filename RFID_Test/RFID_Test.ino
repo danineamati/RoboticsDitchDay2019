3cf78bc108b61b26d962f6122a640e82001574d6
Daniel Neamati <danineamati@gmail.com>
2019-10-19 14:31:02 -0700

    RFID Test now fully functional.

--- RFID_Test/RFID_Test.ino
+++ RFID_Test/RFID_Test.ino
@@ -10,22 +10,28 @@
 #include <SPI.h>
 #include <MFRC522.h>
  
-#define SS_PIN 10
-#define RST_PIN 9
+#define RST_PIN         5          // Configurable, see typical pin layout above
+#define SS_PIN          53         // Configurable, see typical pin layout above
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+// Change to the UID of the card that you want to give access
+String accessUID = "79 0B 38 8E";
  
 void setup() 
 {
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();          // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-  Serial.println("Approximate your card to the reader...");
+  Serial.println("Find your card and bring it to the reader...");
   Serial.println();
 
 }
 void loop() 
 {
-  Serial.println("Start of Loop");
+  // Reset the loop if no new card present on the sensor/reader. 
+  // This saves the entire process when idle.
+//  Serial.println("Start of Loop");
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -36,21 +42,26 @@ void loop()
   {
     return;
   }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+//  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  
   //Show UID on serial monitor
   Serial.print("UID tag :");
-  String content= "";
-  byte letter;
+  String thisUID = "";
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+     thisUID.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     thisUID.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
   Serial.print("Message : ");
-  content.toUpperCase();
-  if (content.substring(1) == "BD 31 15 2B") //change here the UID of the card/cards that you want to give access
+  thisUID.toUpperCase();
+
+  // Substring starts at 1 because there is an extra space
+  if (thisUID.substring(1) == accessUID)
   {
     Serial.println("Authorized access");
     Serial.println();
